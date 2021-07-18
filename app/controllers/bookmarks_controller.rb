c19e3e336d5ebef3e7bdba28fd00ac0894a3a2ad
momoka0508 <y.mmk.0508@gmail.com>
2021-07-18 05:03:31 +0000

    [Start]bookmark_ajax

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,14 +4,12 @@ class BookmarksController < ApplicationController
     it_word = ItWord.find(params[:it_word_id])
     bookmark = current_user.bookmarks.new(it_word_id: it_word.id)
     bookmark.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     it_word = ItWord.find(params[:it_word_id])
     bookmark = current_user.bookmarks.find_by(it_word_id: it_word.id)
     bookmark.destroy
-    redirect_back(fallback_location: root_path)
   end
 
   def index
