fae706ab82c4497f65a511cfdfa61cc103ee3b50
momoka0508 <y.mmk.0508@gmail.com>
2021-07-18 08:11:15 +0000

    [Finish]bookmark_ajax

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,16 @@
 class BookmarksController < ApplicationController
 
   def create
-    it_word = ItWord.find(params[:it_word_id])
-    bookmark = current_user.bookmarks.new(it_word_id: it_word.id)
+    # js.erbファイルへ渡す必要があるからインスタンス変数
+    @it_word = ItWord.find(params[:it_word_id])
+    bookmark = current_user.bookmarks.new(it_word_id: @it_word.id)
     bookmark.save
   end
 
   def destroy
-    it_word = ItWord.find(params[:it_word_id])
-    bookmark = current_user.bookmarks.find_by(it_word_id: it_word.id)
+    # js.erbファイルへ渡す必要があるからインスタンス変数
+    @it_word = ItWord.find(params[:it_word_id])
+    bookmark = current_user.bookmarks.find_by(it_word_id: @it_word.id)
     bookmark.destroy
   end
 
