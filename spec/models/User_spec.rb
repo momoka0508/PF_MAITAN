acac1cd4d5aea41569790530acc376a8c6bfac68
momoka0508 <y.mmk.0508@gmail.com>
2021-08-19 08:06:03 +0000

    [Finish]User_RSpec

---
+++ spec/models/User_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "ユーザー名があるかどうか" do
+    @user = User.new()
+    @user.valid?
+    expect(@user.errors.messages[:name]).to include("can't be blank")
+  end
+end
