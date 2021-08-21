94b65c26baddb64477190e33316839f965ad6636
momoka0508 <y.mmk.0508@gmail.com>
2021-08-21 00:55:54 +0000

    [Update]rubocop

--- test/controllers/study_quantities_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class StudyQuantitiesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get create' do
-    get study_quantities_create_url
-    assert_response :success
-  end
-end
--- test/models/it_word_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ItWordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
